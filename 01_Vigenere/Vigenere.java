14f62d3a015297a3f2264a5186649be262ab8feb
Arne Brüsch <a.bruesch@tu-bs.de>
2017-04-26 11:31:01 +0200

    added files

---
+++ 01_Vigenere/Vigenere.java
@@ -0,0 +1,20 @@
+import de.tubs.cs.iti.jcrypt.chiffre.Cipher;
+
+public class Vigenere extends Cipher {
+
+    public void encipher(BufferedReader text,BufferedWriter ciphertext) {
+    }
+    
+    public void decipher(BufferedReader ciphertext, BufferedWriter text) {
+    }
+
+    public void breakCipher(BufferedReader ciphertext, BufferedWriter text) {
+    }
+
+    public void makeKey() {
+    }
+    public void readKey(BufferedReader key) {
+    }
+    public void writeKey(BufferedWriter key) {
+    }
+}
---
+++ 01_Vigenere/src/task1/Vigenere.java
@@ -0,0 +1,183 @@
+/*
+ * jCrypt - Programmierumgebung für das Kryptologie-Praktikum
+ * Studienarbeit am Institut für Theoretische Informatik der
+ * Technischen Universität Braunschweig
+ * 
+ * Datei:        Vigenere.java
+ * Beschreibung: Dummy-Implementierung der Vigenère-Chiffre
+ * Erstellt:     30. März 2010
+ * Autor:        Martin Klußmann
+ */
+
+package task1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+import de.tubs.cs.iti.jcrypt.chiffre.Cipher;
+
+/**
+ * Dummy-Klasse für die Vigenère-Chiffre.
+ *
+ * @author Martin Klußmann
+ * @version 1.0 - Tue Mar 30 15:53:38 CEST 2010
+ */
+public class Vigenere extends Cipher {
+
+
+  private int[] keyword;
+  public Vigenere() {
+  }
+  /**
+   * Analysiert den durch den Reader <code>ciphertext</code> gegebenen
+   * Chiffretext, bricht die Chiffre bzw. unterstützt das Brechen der Chiffre
+   * (ggf. interaktiv) und schreibt den Klartext mit dem Writer
+   * <code>cleartext</code>.
+   *
+   * @param ciphertext
+   * Der Reader, der den Chiffretext liefert.
+   * @param cleartext
+   * Der Writer, der den Klartext schreiben soll.
+   */
+  public void breakCipher(BufferedReader ciphertext, BufferedWriter cleartext) {
+
+  }
+
+  /**
+   * Entschlüsselt den durch den Reader <code>ciphertext</code> gegebenen
+   * Chiffretext und schreibt den Klartext mit dem Writer
+   * <code>cleartext</code>.
+   *
+   * @param ciphertext
+   * Der Reader, der den Chiffretext liefert.
+   * @param cleartext
+   * Der Writer, der den Klartext schreiben soll.
+   */
+  public void decipher(BufferedReader ciphertext, BufferedWriter cleartext) {
+
+  }
+
+  /**
+   * Verschlüsselt den durch den Reader <code>cleartext</code> gegebenen
+   * Klartext und schreibt den Chiffretext mit dem Writer
+   * <code>ciphertext</code>.
+   * 
+   * @param cleartext
+   * Der Reader, der den Klartext liefert.
+   * @param ciphertext
+   * Der Writer, der den Chiffretext schreiben soll.
+   */
+  public void encipher(BufferedReader cleartext, BufferedWriter ciphertext) {
+      // acquire cleartext and turn into internal representation
+      while
+
+
+
+  }
+
+  /**
+   * Erzeugt einen neuen Schlüssel.
+   * 
+   * @see #readKey readKey
+   * @see #writeKey writeKey
+   */
+  public void makeKey() {
+
+    BufferedReader standardInput = launcher.openStandardInput();
+    boolean accepted = false;
+    String msg = "Geeignete Werte für den Modulus werden in der Klasse "
+        + "'CharacterMapping'\nfestgelegt. Probieren Sie ggf. einen Modulus "
+        + "von 26, 27, 30 oder 31.\nDie Verschiebung muß größer oder gleich 0 "
+        + "und kleiner als der gewählte\nModulus sein.";
+    System.out.println(msg);
+    // Frage jeweils solange die Eingabe ab, bis diese akzeptiert werden kann.
+    do {
+      System.out.print("Geben Sie den Modulus ein: ");
+      try {
+        modulus = Integer.parseInt(standardInput.readLine());
+        if (modulus < 1) {
+          System.out.println("Ein Modulus < 1 wird nicht akzeptiert. Bitte "
+              + "korrigieren Sie Ihre Eingabe.");
+        } else {
+          // Prüfe, ob zum eingegebenen Modulus ein Default-Alphabet existiert.
+          String defaultAlphabet = CharacterMapping.getDefaultAlphabet(modulus);
+          if (!defaultAlphabet.equals("")) {
+            msg = "Vordefiniertes Alphabet: '" + defaultAlphabet
+                + "'\nDieses vordefinierte Alphabet kann durch Angabe einer "
+                + "geeigneten Alphabet-Datei\nersetzt werden. Weitere "
+                + "Informationen finden Sie im Javadoc der Klasse\n'Character"
+                + "Mapping'.";
+            System.out.println(msg);
+            accepted = true;
+          } else {
+            msg = "Warnung: Dem eingegebenen Modulus kann kein Default-"
+                + "Alphabet zugeordnet werden.\nErstellen Sie zusätzlich zu "
+                + "dieser Schlüssel- eine passende Alphabet-Datei.\nWeitere "
+                + "Informationen finden Sie im Javadoc der Klasse 'Character"
+                + "Mapping'.";
+            System.out.println(msg);
+            accepted = true;
+          }
+        }
+      } catch (NumberFormatException e) {
+        System.out.println("Fehler beim Parsen des Modulus. Bitte korrigieren"
+            + " Sie Ihre Eingabe.");
+      } catch (IOException e) {
+        System.err
+            .println("Abbruch: Fehler beim Lesen von der Standardeingabe.");
+        e.printStackTrace();
+        System.exit(1);
+      }
+    } while (!accepted);
+    accepted = false;
+    do {
+      try {
+        System.out.print("Geben Sie das Schlüsselwort ein: ");
+        String keyString = standardInput.readLine();
+	// Verify that keyword consists of legal characters only
+	this.keyword = new int[keyword.length()]();
+	// map input to internal alphabet
+	for (int i =0;i<keyword.length();i++) {
+	    mapped_char = charMap.mapChar(keyString.charAt[i]);
+	    if ((mapped_char == '·') || (mapped_char == -1)) {
+		// need to throw some fitting exception
+		throw new NumberFormatException();
+	    } else {
+		key[i] = mapped_char;
+	}
+        accepted = true;
+      } catch (NumberFormatException e) {
+        System.out.println("Ungültiges Schlüsselwort.");
+      } catch (IOException e) {
+        System.err
+            .println("Abbruch: Fehler beim Lesen von der Standardeingabe.");
+        e.printStackTrace();
+        System.exit(1);
+      }
+    } while (!accepted);
+  }
+
+  /**
+   * Liest den Schlüssel mit dem Reader <code>key</code>.
+   * 
+   * @param key
+   * Der Reader, der aus der Schlüsseldatei liest.
+   * @see #makeKey makeKey
+   * @see #writeKey writeKey
+   */
+  public void readKey(BufferedReader key) {
+
+  }
+
+  /**
+   * Schreibt den Schlüssel mit dem Writer <code>key</code>.
+   * 
+   * @param key
+   * Der Writer, der in die Schlüsseldatei schreibt.
+   * @see #makeKey makeKey
+   * @see #readKey readKey
+   */
+  public void writeKey(BufferedWriter key) {
+
+  }
+}
